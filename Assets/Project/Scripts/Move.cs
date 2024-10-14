0ae04c5af15ce5d039d9a772c6faeae8144ab079
s1f10220225 <107395363+s1f10220225@users.noreply.github.com>
2024-10-15 00:24:08 +0900

    旋回の不具合を修正

--- Assets/Project/Scripts/Move.cs
+++ Assets/Project/Scripts/Move.cs
@@ -6,14 +6,15 @@ public class Move : MonoBehaviour
 {
     public Transform targetObject;     // 追跡する対象オブジェクト
     public float moveSpeed = 5f;       // 前方への移動速度
-    public float rotationScale = 0.1f; // 旋回速度のスケール
+
+    // 旋回速度のスケール
+    public float rotationScale_X = 1f;
+    public float rotationScale_Z = 0.5f;
 
     public Rigidbody rb;              // Rigidbodyコンポーネントへの参照
 
     void Start()
     {
-        // Rigidbodyコンポーネントを取得
-        rb = GetComponent<Rigidbody>();
     }
 
     void Update()
@@ -26,20 +27,53 @@ public class Move : MonoBehaviour
         // 前方へ一定速度で移動
         rb.MovePosition(rb.position + transform.forward * moveSpeed * Time.fixedDeltaTime);
 
-        // 対象オブジェクトのZ軸の回転角度を取得
-        float targetZRotation = targetObject.eulerAngles.z;
 
-        // UnityのeulerAnglesは0 ~ 360の範囲で返すため、-180 ~ 180で計算するように調整
-        if (targetZRotation > 180)
-        {
-            targetZRotation -= 360;
-        }
+        // // 対象オブジェクトのX軸の回転角度を取得
+        // float targetXRotation = targetObject.eulerAngles.x;
+
+        // // UnityのeulerAnglesは0 ~ 360の範囲で返すため、-180 ~ 180で計算するように調整
+        // if (targetXRotation > 180)
+        // {
+        //     targetXRotation -= 360;
+        // }
+
+        // // 対象オブジェクトのZ軸の回転角度を取得
+        // float targetZRotation = targetObject.eulerAngles.z;
+
+
+
+        // // UnityのeulerAnglesは0 ~ 360の範囲で返すため、-180 ~ 180で計算するように調整
+        // if (targetZRotation > 180)
+        // {
+        //     targetZRotation -= 360;
+        // }
+
+        // 対象オブジェクトのローカルから見た回転角度を取得
+        Vector3 targetLocalEulerAngles = targetObject.localEulerAngles;
+
+
+        // 値範囲を制限
+        float targetXRotation = NormalizeAngle(targetLocalEulerAngles.x);
+        float targetZRotation = NormalizeAngle(targetLocalEulerAngles.z);
 
         // 角度に応じた旋回速度を計算
-        float rotationSpeed = targetZRotation * rotationScale;
+        float xRotationSpeed = targetXRotation * rotationScale_X;
+        float zRotationSpeed = targetZRotation * rotationScale_Z;
 
-        // 旋回を適用 (Quaternionを用いて回転を適用)
-        Quaternion deltaRotation = Quaternion.Euler(Vector3.up * -rotationSpeed * Time.fixedDeltaTime);
-        rb.MoveRotation(rb.rotation * deltaRotation);
+
+        // 旋回
+        Quaternion deltaRotationX = Quaternion.Euler(Vector3.right * xRotationSpeed * Time.fixedDeltaTime);
+        Quaternion deltaRotationZ = Quaternion.Euler(Vector3.up * -zRotationSpeed * Time.fixedDeltaTime);
+        rb.MoveRotation(rb.rotation * deltaRotationZ * deltaRotationX);
+    }
+
+    // 角度を-180から180の範囲に補正する(上下反転対策)
+    float NormalizeAngle(float angle)
+    {
+        if (angle > 180f)
+        {
+            return angle - 360f;
+        }
+        return angle;
     }
 }
