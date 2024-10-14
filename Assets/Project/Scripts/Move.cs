7a1edd0342e3066045466d6ec6d96984012781c5
s1f10220225 <s1F102202257@iniad.org>
2024-10-14 16:19:34 +0900

    物理演算用に各種設定、スクリプトを変更

--- Assets/Project/Scripts/Move.cs
+++ Assets/Project/Scripts/Move.cs
@@ -8,17 +8,23 @@ public class Move : MonoBehaviour
     public float moveSpeed = 5f;       // 前方への移動速度
     public float rotationScale = 0.1f; // 旋回速度のスケール
 
-    // Start is called before the first frame update
+    public Rigidbody rb;              // Rigidbodyコンポーネントへの参照
+
     void Start()
+    {
+        // Rigidbodyコンポーネントを取得
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
     {
 
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        // 前方への一定速度での移動
-        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        // 前方へ一定速度で移動
+        rb.MovePosition(rb.position + transform.forward * moveSpeed * Time.fixedDeltaTime);
 
         // 対象オブジェクトのZ軸の回転角度を取得
         float targetZRotation = targetObject.eulerAngles.z;
@@ -32,7 +38,8 @@ public class Move : MonoBehaviour
         // 角度に応じた旋回速度を計算
         float rotationSpeed = targetZRotation * rotationScale;
 
-        // 旋回を適用
-        transform.Rotate(Vector3.up, -rotationSpeed * Time.deltaTime);
+        // 旋回を適用 (Quaternionを用いて回転を適用)
+        Quaternion deltaRotation = Quaternion.Euler(Vector3.up * -rotationSpeed * Time.fixedDeltaTime);
+        rb.MoveRotation(rb.rotation * deltaRotation);
     }
 }
