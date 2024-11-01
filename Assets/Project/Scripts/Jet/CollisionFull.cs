a58d48a6d314418c23515fcad7881a97b1b6bbc6
s1f10220225 <s1F102202257@iniad.org>
2024-11-01 16:55:00 +0900

    タグの調整

--- Assets/Project/Scripts/Jet/CollisionFull.cs
+++ Assets/Project/Scripts/Jet/CollisionFull.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class CollisionFull : MonoBehaviour
 {
+    public static CollisionFull Instance { get; private set; }
     public GameObject obj;
     void OnCollisionEnter(Collision col)
     {
-        if (col.gameObject.CompareTag("item"))
+        if (col.gameObject.CompareTag("l_hand"))
         {
-            // Burst(col);
+            Burst(col);
         }
     }
 
-    // void Burst(col)
-    // {
-    //     Destroy(col);
-    // }
+
+    void Burst(Collision col)
+    {
+        Destroy(col.gameObject);
+    }
 }
